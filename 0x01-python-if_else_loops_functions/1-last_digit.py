10b462542aaf618077fd75812d4b1c6c33ffa657
Wheya <rejoicesimon7@gmail.com>
2022-11-29 18:46:00 -0800

    if/else, loops, functions

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number >= 0:
-    last_digit = number % 10
+if number < 0:
+    last_digit = number % -10
 else:
-    last_digit = ((number * -1) % 10) * -1
-
-    message = "Last digit of %d is %d and is" % (number, last_digit)
-
-    if last_digit == 0:
-        print(message, "0")
-    elif last_digit > 5:
-        print(message, "greater than 5")
-    else:
-        print(message, "less than 6 and not 0")
+    last_digit = number % 10
+    print('Last digit of {} is '.format(number), end="")
+    if lastd > 5:
+        print('{} and is greater than 5'.format(lastd))
+    elif lastd == 0:
+        print('{} and is 0'.format(lastd))
+    elif lastd < 6 and lastd != 0:
+        print('{} and is less than 6 and not 0'.format(lastd))
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-def to_uper(character):
-    if ord(character) >= 97 and ord(character) <= 122:
-        return (ord(character) - 32)
-    else:
-        return ord(character)
 
 
-    def uppercase(string):
-        string_new = ""
-        for character in string:
-            string_new += "%c" % to_uper(character)
-        print("{:s}".format(string_new))
+def uppercase(str):
+    for i in str:
+        tmp = ord(i)
+        if tmp >= 97 and tmp <= 122:
+            pr = chr(tmp - 32)
+        else:
+            pr = i
+        print('{}'.format(pr), end="")
+    print()
