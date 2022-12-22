b408edfa5cfb6770717baf1acb3f113d02aca1a2
Wheya <rejoicesimon7@gmail.com>
2022-12-21 18:27:28 -0800

    Classes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -26,6 +26,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-        def area(self):
-            """Return the current area of the square."""
-            return (self.__size * self.__size)
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
